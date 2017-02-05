f4218860a9501f052fce224184b0ef00d44e4483
Andrew Faraday <ajfaraday@edge-ka.com>
2017-02-05 18:14:08 +0000

    Tileset and window stuffage

---
+++ data/tilesets/frame.js
@@ -0,0 +1,18 @@
+if(typeof data === 'undefined') {
+  data = {};
+}
+if(typeof data.tilesets === 'undefined') {
+  data.tilesets = {};
+}
+
+data.tilesets.frame = {
+  'top-left': new Image([200472, 807428192], 8, 8),
+  top: new Image([16776960, 0], 8, 8),
+  'top-right': new Image([12644376, 201590278], 8, 8),
+  right: new Image([101058054, 101058054], 8, 8),
+  'bottom-right': new Image([101057548, 418430976], 8, 8),
+  bottom: new Image([0, 16776960], 8, 8),
+  'bottom-left': new Image([1616912432, 403636992], 8, 8),
+  left: new Image([1616928864, 1616928864], 8, 8)
+};
+
